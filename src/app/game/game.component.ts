de897e274ed890686bdee5feeeb7f4ec6b088534
Pl4tinZ <109986183+Pl4tinZ@users.noreply.github.com>
2023-01-17 13:23:04 +0100

    create playground with cards and animations

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Game } from 'src/models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent {
+  pickCardAnimation = false;
+  game: Game;
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+
+}
